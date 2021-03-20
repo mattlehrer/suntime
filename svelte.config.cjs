a38f65ddfdcfbde55f4a2766502fb83417adb6b4
Matt Lehrer <lehrerm@gmail.com>
2021-03-19 22:28:58 -0700

    chore: update vite config

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,4 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess');
+const pkg = require('./package.json');
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -18,5 +20,11 @@ module.exports = {
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+
+		vite: {
+			ssr: {
+				noExternal: Object.keys(pkg.dependencies || {}),
+			},
+		},
 	},
 };
