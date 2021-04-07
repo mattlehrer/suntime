58e52097e2aaaca106461a7efbd8eee3cc9b9df7
Matt Lehrer <lehrerm@gmail.com>
2021-04-07 11:43:57 -0700

    wip: setup boxes

---
+++ src/lib/timeboxes.ts
@@ -0,0 +1,110 @@
+import { sun } from '@modern-dev/daylight';
+import { add, areIntervalsOverlapping, compareAsc } from 'date-fns';
+
+export type TimeBox = {
+	name: string;
+	start: Date;
+	end: Date;
+};
+
+export const timeboxes = ({
+	date,
+	lat,
+	long,
+}: {
+	date: Date;
+	lat: number;
+	long: number;
+}): TimeBox[] => {
+	// date will be in one of 11 boxes:
+	// night,
+	// astronomicalDawn,
+	// nauticalDawn,
+	// civilDawn,
+	// sunrise,
+	// morning,
+	// afternoon,
+	// sunset,
+	// civilDusk,
+	// nauticalDusk,
+	// astronomicalDusk,
+
+	// and one box will be duplicated across the top and bottom of the screen when running from 12 hours before to 12 hours after the current time
+
+	const twelveHoursBefore = add(date, { hours: -12 });
+	const twelveHourseFromNow = add(date, { hours: 12 });
+	const outputRange: TimeSpan = {
+		start: twelveHoursBefore,
+		end: twelveHourseFromNow,
+	};
+
+	let output: TimeBox[] = [];
+
+	let yesterdaysTimes: { [key: string]: TimeSpan } = {};
+	let tomorrowsTimes: { [key: string]: TimeSpan } = {};
+	let todaysTimes: { [key: string]: TimeSpan } = {};
+	let transit: Date, dawn: Date, dusk: Date;
+
+	// transit, dawn, and dusk are Dates and the rest are TimeSpans
+	({ transit, dawn, dusk, ...todaysTimes } = sun.getTimes(date, lat, long));
+	// add morning and afternoon periods
+	todaysTimes = {
+		...todaysTimes,
+		morning: { start: todaysTimes.sunrise.end, end: transit },
+		afternoon: { start: transit, end: todaysTimes.sunset.start },
+	};
+
+	({ transit, dawn, dusk, ...yesterdaysTimes } = sun.getTimes(
+		add(date, { days: -1 }),
+		lat,
+		long,
+	));
+	yesterdaysTimes = {
+		...yesterdaysTimes,
+		morning: { start: yesterdaysTimes.sunrise.end, end: transit },
+		afternoon: { start: transit, end: yesterdaysTimes.sunset.start },
+	};
+
+	({ transit, dawn, dusk, ...tomorrowsTimes } = sun.getTimes(
+		add(date, { days: 1 }),
+		lat,
+		long,
+	));
+	tomorrowsTimes = {
+		...tomorrowsTimes,
+		morning: { start: tomorrowsTimes.sunrise.end, end: transit },
+		afternoon: { start: transit, end: tomorrowsTimes.sunset.start },
+	};
+
+	// add night to today, needs to reference other days
+	todaysTimes = {
+		...todaysTimes,
+		preDawn: {
+			start: yesterdaysTimes.astronomicalDusk.end,
+			end: todaysTimes.astronomicalDawn.start,
+		},
+		postDusk: {
+			start: todaysTimes.astronomicalDusk.end,
+			end: tomorrowsTimes.astronomicalDawn.start,
+		},
+	};
+
+	output = [
+		...Object.entries(yesterdaysTimes)
+			.filter(([, span]) => areIntervalsOverlapping(outputRange, span))
+			.map(([name, span]) => ({ name, ...span })),
+		...Object.entries(todaysTimes)
+			.filter(([, span]) => areIntervalsOverlapping(outputRange, span))
+			.map(([name, span]) => ({ name, ...span })),
+		...Object.entries(tomorrowsTimes)
+			.filter(([, span]) => areIntervalsOverlapping(outputRange, span))
+			.map(([name, span]) => ({ name, ...span })),
+	].sort(({ end: aEnd }, { end: bEnd }) => compareAsc(aEnd, bEnd));
+
+	return output;
+};
+
+type TimeSpan = {
+	start: Date;
+	end: Date;
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,5 @@
 <script>
-	import type { SunTimes } from '@modern-dev/daylight';
-	import { sun } from '@modern-dev/daylight';
+	import { timeboxes } from '$lib/timeboxes';
 	import { format } from 'date-fns';
 	import { onMount } from 'svelte';
 
@@ -10,14 +9,8 @@
 	let lat: number;
 	let long: number;
 	let city: string;
-	let sunTimes: SunTimes;
-	let times: HTMLElement;
+	let timeBoxes: HTMLElement;
 	let innerHeight: number;
-	let innerWidth: number;
-	let timeTuples: {
-		text: string;
-		time: Date;
-	}[];
 
 	onMount(async () => {
 		try {
@@ -36,113 +29,9 @@
 			long = -122.3029;
 			city = 'Seattle';
 
-			sunTimes = sun.getTimes(date, lat, long);
-			const {
-				astronomicalDawn,
-				nauticalDawn,
-				civilDawn,
-				sunrise,
-				transit,
-				sunset,
-				civilDusk,
-				nauticalDusk,
-				astronomicalDusk,
-			} = sunTimes;
-			console.log(sunTimes);
-
-			// times = document.getElementById('times');
-
-			timeTuples = [
-				{ text: 'Astronomical Dawn Start', time: astronomicalDawn.start },
-				{ text: 'Nautical Dawn Start', time: nauticalDawn.start },
-				{ text: 'Civil Dawn Start', time: civilDawn.start },
-				{
-					text: 'Sunrise',
-					time: new Date(
-						sunrise.start.getTime() +
-							(sunrise.end.getTime() - sunrise.start.getTime()) / 2,
-					),
-				},
-				{ text: 'Solar Noon', time: transit },
-				{
-					text: 'Sunset',
-					time: new Date(
-						sunset.start.getTime() +
-							(sunset.end.getTime() - sunset.start.getTime()) / 2,
-					),
-				},
-				{ text: 'Civil Dusk End', time: civilDusk.end },
-				{ text: 'Nautical Dusk End', time: nauticalDusk.end },
-				{ text: 'Astronomical Dusk End', time: astronomicalDusk.end },
-			];
-
-			timeTuples.forEach(({ text, time }) => {
-				const div = document.createElement('hr');
-				div.classList.add(
-					'absolute',
-					'w-full',
-					'text-white',
-					'h-px',
-					'border-0',
-				);
-				div.setAttribute('id', text);
-				// const div = document.createElement('div');
-				// div.classList.add(
-				// 	'flex',
-				// 	'flex-row',
-				// 	'justify-between',
-				// 	'absolute',
-				// 	'w-full',
-				// 	'text-sm',
-				// 	'sm:text-base',
-				// 	'px-4',
-				// 	'sm:px-8',
-				// );
-
-				// const textP = document.createElement('p');
-				// textP.innerText = text;
-				// const timeP = document.createElement('p');
-				// timeP.innerText = '>';
-				// timeP.setAttribute(
-				// 	'title',
-				// 	time.toLocaleTimeString([], {
-				// 		hour: '2-digit',
-				// 		minute: '2-digit',
-				// 		second: '2-digit',
-				// 	}),
-				// );
-				// timeP.innerText = time.toLocaleTimeString([], {
-				// 	hour: '2-digit',
-				// 	minute: '2-digit',
-				// 	second: '2-digit',
-				// });
-				// div.appendChild(textP);
-				// div.appendChild(timeP);
-
-				// calculate time since midnight for now
-				// TODO: switch order to center current time?
-				const sinceMidnight = getMsSinceMidnight(time);
-				const dayMs = 24 * 60 * 60 * 1000 * 1.03; // +3% so times at bottom edge are on screen
-				const percentOfDay = sinceMidnight / dayMs;
-				div.style.top = `${percentOfDay * innerHeight}px;`;
-				// div.style['mix-blend-mode'] = 'difference';
-				// div.setAttribute('style', `'mix-blend-mode': difference`);
-
-				if (text === 'Solar Noon') {
-					div.classList.add(
-						'bg-gradient-to-r',
-						'from-current',
-						'via-transparent',
-						'to-current',
-					);
-				} else {
-					div.classList.add('bg-white');
-					div.style['mix-blend-mode'] = 'difference';
-					// div.setAttribute('style', `'mix-blend-mode': difference`);
-				}
-
-				times.appendChild(div);
-			});
+			date = new Date();
+			const boxes = timeboxes({ date, lat, long });
+			console.log({ boxes });
 		} catch (error) {
 			console.error(error);
 		}
@@ -159,37 +48,15 @@
 			clearInterval(interval);
 		};
 	});
-
-	$: innerHeight &&
-		timeTuples.forEach(({ text, time }) => {
-			const div = document.getElementById(text);
-
-			// calculate time since midnight for now
-			// TODO: switch order to center current time
-			const sinceMidnight = getMsSinceMidnight(time);
-			const dayMs = 24 * 60 * 60 * 1000 * 1.03; // +3% so times at bottom edge are on screen
-			const percentOfDay = sinceMidnight / dayMs;
-			// console.log({ text, percentOfDay, innerHeight });
-
-			div.setAttribute('style', `top: ${percentOfDay * innerHeight}px`);
-		});
-
-	function getMsSinceMidnight(d: Date) {
-		const midnight = new Date(d);
-		midnight.setHours(0, 0, 0, 0);
-		return d.getTime() - midnight.getTime();
-	}
 </script>
 
 <svelte:head>
 	<title>Sun Time</title>
 </svelte:head>
 
-<svelte:window bind:innerHeight bind:innerWidth />
+<svelte:window bind:innerHeight />
 
-<div class="relative h-full">
-	<div id="bg" class="fixed inset-0 h-full" />
-
+<div class="relative h-full bg-black">
 	<main class="absolute inset-0 h-full text-gray-50">
 		<div
 			id="clock"
@@ -213,23 +80,7 @@
 		<div
 			class="text-yellow-400 mix-blend-difference"
 			id="times"
-			bind:this={times}
+			bind:this={timeBoxes}
 		/>
 	</main>
 </div>
-
-<style>
-	#bg {
-		background-image: linear-gradient(
-			to bottom,
-			rgba(249, 251, 240, 1) 10%,
-			rgba(215, 253, 254, 1) 15%,
-			rgba(167, 222, 253, 1) 20%,
-			rgb(76, 122, 178) 30%,
-			rgb(59, 96, 141) 35%,
-			rgb(41, 67, 100) 40%,
-			rgb(8, 13, 20) 100%
-		);
-		/* opacity: 0.75; */
-	}
-</style>
