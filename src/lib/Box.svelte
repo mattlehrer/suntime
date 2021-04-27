3f57c1b8d948796a1ce9d3783df2cbe0ec730ed9
Matt Lehrer <lehrerm@gmail.com>
2021-04-27 10:06:24 -0700

    restyle

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <div
-	class="h-full"
+	class="h-full anim"
 	style={gradient}
 	on:mouseenter={() => (showDetails = true)}
 	on:mouseleave={() => (showDetails = false)}
@@ -20,19 +20,41 @@
 >
 	{#if showDetails}
 		<div
-			class="mix-blend-difference px-4 flex flex-col justify-between h-full items-end"
+			class="mix-blend-difference flex justify-between h-full w-full items-end relative"
 		>
-			<div class="flex justify-between w-full">
+			<div class="flex flex-col justify-center h-full ml-4">
 				<p class="tabular-nums">
 					{text}
 				</p>
+			</div>
+			<div class="flex flex-col justify-between h-full center mr-4">
 				<p class="tabular-nums">
 					{format(start, 'h:mm:ss')}
 				</p>
+				<p class="tabular-nums">
+					{format(end, 'h:mm:ss')}
+				</p>
 			</div>
-			<p class="tabular-nums">
-				{format(end, 'h:mm:ss')}
-			</p>
 		</div>
 	{/if}
 </div>
+
+<style>
+	.center {
+		position: absolute;
+		top: 50%;
+		right: 0;
+		transform: translate(0, -50%);
+		min-height: 3.75rem;
+	}
+
+	/* @keyframes Box-highlight {
+		to {
+			background: red;
+		}
+	} */
+
+	/* .anim:hover {
+		animation: Box-highlight infinite 3s ease-in-out alternate;
+	} */
+</style>
